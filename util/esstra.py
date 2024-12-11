1ce799d813254bb1f156cfe1bac6698ae703c54b
NAGAO Sadakazu <sadakazu.nagao@sony.com>
2024-12-11 10:55:21 +0900

    implement "update" command to attach license info

--- util/esstra.py
+++ util/esstra.py
@@ -27,21 +27,27 @@ import sys
 import argparse
 from pathlib import Path
 import subprocess
-import json
 import re
+import tempfile
+
 import yaml
 
+
+DEBUG = False
 
 COMMANDS = {
     'show': 'display information embedded by ESSTRA Core',
     'update': 'update embedded information (*not implemented yet*)',
 }
 
-# keys defined in esstra core
+# keys in esstra_info
 SECTION_NAME = 'esstra_info'
 KEY_INPUT_FILE_NAME = 'InputFileName'
 KEY_SOURCE_PATH = 'SourcePath'
-KEY_SOURCE_FILES = 'SouceFiles'
+KEY_SOURCE_FILES = 'SourceFiles'
+KEY_SHA1 = 'SHA1'
+KEY_LICENSE_INFO = 'LicenseInfo'
+HASH_ALGORITHM = 'SHA1'
 
 # keys for binary file paths
 KEY_BINARY_FILE_NAME = 'BinaryFileName'
@@ -51,6 +57,18 @@ KEY_BINARY_PATH = 'BinaryPath'
 #
 # basic helper functions
 #
+DEBUG = False
+
+
+def debug(msg):
+    if DEBUG:
+        print(f'[DEBUG] {msg}')
+
+
+def message(msg):
+    print(f'* {msg}')
+
+
 def error(msg):
     print(f'[ERROR] {msg}', file=sys.stderr)
 
@@ -66,13 +84,13 @@ def call_function_by_name(function_name, *args):
     return function(*args)
 
 
-def get_resolved_file_path(filename):
-    path = Path(filename).resolve()
+def get_resolved_file_path(tvfile):
+    path = Path(tvfile).resolve()
     if not path.exists():
-        error(f'{filename!r}: not exist')
+        error(f'{tvfile!r}: not exist')
         return None
     if not Path(path).is_file():
-        error(f'{filename!r}: not a file')
+        error(f'{tvfile!r}: not a file')
         return None
     return str(path)
 
@@ -167,13 +185,13 @@ TAG_FILE_CHECKSUM = 'FileChecksum'
 TAG_LICENSE_INFO_IN_FILE = 'LicenseInfoInFile'
 
 
-def _parse_spdx_tv(filename):
+def _parse_spdx_tv(tvfile):
     def extract_multiline_text(tagged_text):
         assert tagged_text.startswith('<text>'), tagged_text
         assert tagged_text.endswith('</text>'), tagged_text
         return tagged_text.replace('<text>', '').replace('</text>', '').strip()
 
-    with open(filename, 'r', encoding='utf-8') as fp:
+    with open(tvfile, 'r', encoding='utf-8') as fp:
         lines = fp.readlines()
 
     tag_values = []
@@ -201,30 +219,132 @@ def _parse_spdx_tv(filename):
                     value += '\n'
                     continue
                 value = extract_multiline_text(value)
-            tag_values.append((tag, value))
+            yield tag, value
+            tag = None
+            value = None
 
-    return tag_values
+    if tag:
+        yield tag, value
 
 
-def _extract_file_information_from_spdx_tv(tag_values):
+def _make_hash_map_from_spdx_tv(tvfile, algo):
+    assert algo[-1] != ':'
+    algo = algo.upper() + ':'
+
     result = {}
-    current_file = None
+    filename = None
+    filehash = None
+    fileinfo = None
 
-    for tag, value in tag_values:
+    def push(fhash, finfo):
+        if fhash not in result.keys():
+            result[fhash] = [finfo]
+        else:
+            result[fhash].append(finfo)
+
+    for tag, value in _parse_spdx_tv(tvfile):
         if tag == TAG_FILE_NAME:
-            current_file = value
-            result[current_file] = {}
-        elif current_file:
+            # flush last item if any
+            if filename:
+                assert filehash
+                push(filehash, fileinfo)
+            # reset item elements
+            filename = value
+            filehash = None
+            fileinfo = [(tag, value)]
+        elif filename:
+            fileinfo.append((tag, value))
             if tag == TAG_FILE_CHECKSUM:
-                match = RE_TAG_VALUE.match(value)
-                assert match
-                algo = match[1]
-                checksum = match[2]
-                result[current_file][algo] = checksum
-            elif tag == TAG_LICENSE_INFO_IN_FILE:
-                result[current_file][tag] = value
+                hash_value_list = value.split(algo)
+                if len(hash_value_list) == 2:
+                    # this is the hash you want
+                    filehash = hash_value_list[-1].strip()
+        else:
+            debug(f'ignored: "{tag}: {value}"')
+
+    # flush last item if any
+    if filename:
+        assert filehash
+        push(filehash, fileinfo)
 
     return result
+
+
+def _get_file_info_by_path(file_info_list, path):
+    for file_info in file_info_list:
+        for tag, value in file_info:
+            if tag == TAG_FILE_NAME:
+                if Path(value).name == Path(path).name:
+                    return file_info
+    return None
+
+
+def _attach_license_info(esstra_info, hash_map):
+    for input_file_elem in esstra_info:
+        assert KEY_INPUT_FILE_NAME in input_file_elem
+        assert KEY_SOURCE_FILES in input_file_elem
+        for file_info in input_file_elem[KEY_SOURCE_FILES]:
+            debug(file_info)
+            path = file_info[KEY_SOURCE_PATH]
+            sha1sum = file_info[KEY_SHA1]
+            if sha1sum not in hash_map:
+                continue
+
+            spdx_file_info = _get_file_info_by_path(hash_map[sha1sum], path)
+            assert spdx_file_info
+
+            # attach license info
+            license_info = [
+                value for tag, value in spdx_file_info
+                if tag == TAG_LICENSE_INFO_IN_FILE
+            ]
+            if license_info:
+                file_info[KEY_LICENSE_INFO] = license_info
+                debug(f'{path}: {license_info}')
+
+
+def _update_esstra_info(binary, esstra_info):
+    with tempfile.NamedTemporaryFile('w', encoding='utf-8') as fp:
+        esstra_info_string = yaml.safe_dump(esstra_info, sort_keys=False)
+        for line in esstra_info_string.splitlines():
+            fp.write(line)
+            fp.write('\0')
+        fp.flush()
+
+        result = subprocess.run(
+            ['objcopy', binary,
+             '--update-section', f'{SECTION_NAME}={fp.name}'],
+            encoding='utf-8',
+            check=False,
+            capture_output=True)
+
+        # check if it was ok or error
+        if result.returncode:
+            error(f'objcopy returned code {result.returncode}')
+            error(result.stderr)
+            return False
+
+    return True
+
+
+def _create_backup_file(src, dst, overwrite):
+    if not overwrite and Path(dst).exists():
+        error(f'{dst!r}: exists.')
+        return False
+
+    result = subprocess.run(
+        ['cp', '-a', src, dst],
+        encoding='utf-8',
+        check=False,
+        capture_output=True)
+
+    # check if it was ok or error
+    if result.returncode:
+        error(f'cp returned code {result.returncode}')
+        error(result.stderr)
+        return False
+
+    return True
 
 
 #
@@ -232,21 +352,54 @@ def _extract_file_information_from_spdx_tv(tag_values):
 #
 def _setup_update(parser):
     parser.add_argument(
-        '-l', '--license-info', required=True,
+        '-i', '--info-file', required=True,
+        nargs='+',
+        action='extend',
         help='spdx tag/value file containing license information')
     parser.add_argument(
         'binary', nargs='+',
         help='ESSTRA-built binary file to update embedded information')
+    parser.add_argument(
+        '--no-backup',
+        action='store_true',
+        help='do not create backup of binary file')
+    parser.add_argument(
+        '--overwrite-backup',
+        action='store_true',
+        help='overwrite old backup file')
+    parser.add_argument(
+        '-s', '--suffix',
+        default='bak',
+        help='suffix of backup file')
 
 
 #
 # run 'update' command
 #
 def _run_update(args):
-    tag_values = _parse_spdx_tv(args.license_info)
-    result = _extract_file_information_from_spdx_tv(tag_values)
+    errors = 0
+    for binary in args.binary:
+        if binary.endswith(f'.{args.suffix}'):
+            message(f'skip backup file {binary!r}.')
+        message(f'processing {binary!r}...')
+        for spdx_tv_file in args.info_file:
+            esstra_info = _extract_esstra_info(binary)
+            hash_map = _make_hash_map_from_spdx_tv(spdx_tv_file, HASH_ALGORITHM)
+            _attach_license_info(esstra_info, hash_map)
+            debug(yaml.dump(esstra_info))
 
-    print(json.dumps(result, indent=4))
+            if not args.no_backup:
+                if not _create_backup_file(
+                        binary, f'{binary}.{args.suffix}',
+                        args.overwrite_backup):
+                    error('cannot create backup. skip')
+                    errors += 1
+            _update_esstra_info(binary, esstra_info)
+
+    if errors:
+        message(f'done with {errors} error(s).')
+    else:
+        message('done.')
 
     return 0
 
@@ -280,6 +433,9 @@ def main():
         call_function_by_name(f'_setup_{name}', subparser)
 
     args = parser.parse_args()
+
+    global DEBUG
+    DEBUG = args.debug
 
     if not hasattr(args, 'name'):
         print('no command specified. show help by "-h".')
