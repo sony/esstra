7560835881de3a5601edb66627d763667c9c214d
NAGAO Sadakazu <sadakazu.nagao@sony.com>
2025-05-21 10:09:56 +0900

    change section name from esstra_info to .esstra

--- util/esstra.py
+++ util/esstra.py
@@ -54,7 +54,7 @@ def error(msg):
 
 
 class MetadataHandler:
-    SECTION_NAME = 'esstra_info'
+    SECTION_NAME = '.esstra'
     KEY_SOURCE_FILES = 'SourceFiles'
     KEY_FILE = 'File'
     KEY_LICENSE_INFO = 'LicenseInfo'
@@ -143,7 +143,7 @@ class MetadataHandler:
 
     def __exists_metadata(self, bianry_path):
         result = self.__run_command(
-            f'readelf -SW {bianry_path} | grep {self.SECTION_NAME}')
+            f'readelf -SW {bianry_path} | fgrep {self.SECTION_NAME}')
 
         if result.returncode:
             return False
