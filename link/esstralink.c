5bfc6aec8e0a06709ee31f20b0f80f06c262572f
NAGAO Sadakazu <sadakazu.nagao@sony.com>
2025-12-04 09:55:32 +0900

    add verbosity support to esstra link

--- link/esstralink.c
+++ link/esstralink.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <stdarg.h>
 #include <sys/wait.h>
 #include <linux/limits.h>
 #include "plugin-api.h"
@@ -23,7 +24,7 @@
 static const char *tool_name = "ESSTRA Link";
 static const char *tool_version = "0.5.0";
 
-static ld_plugin_message message;
+static ld_plugin_message plugin_message = NULL;
 static const char *link_output_name = NULL;
 
 #define ESSTRA_UTIL_COMMAND "esstra"
@@ -32,6 +33,34 @@ static const char *link_output_name = NULL;
 static bool exists_shrink_option = false;
 static char shrink_rule[ARG_MAX - sizeof(FILE_PREFIX_MAP_OPTION) - 2]; /* '2' is for '=' and '\0' */
 
+enum MessageLevel {
+    L_DEBUG  = 1U,
+    L_INFO   = 1U << 2,
+    L_NOTICE = 1U << 3,
+    L_ERROR  = 1U << 4,
+};
+static uint8_t messages_to_show = L_ERROR | L_NOTICE;
+#define MSG_LEN 1024
+
+/*
+ * message
+ */
+static void
+message(enum MessageLevel level, const char* format, ...) {
+    if (plugin_message == NULL) return;
+    if ((messages_to_show & level) == 0) return;
+
+    char msg[MSG_LEN];
+    va_list args;
+    va_start(args, format);
+    vsnprintf(msg, MSG_LEN, format, args);
+    va_end(args);
+
+    (void)tool_version;
+    enum ld_plugin_level lv = level & L_ERROR ? LDPL_ERROR : LDPL_INFO;
+    plugin_message(lv, msg);
+}
+
 
 /*
  * CLEANUP HOOK - aggregates metadata
@@ -39,13 +68,13 @@ static char shrink_rule[ARG_MAX - sizeof(FILE_PREFIX_MAP_OPTION) - 2]; /* '2' is
 static enum ld_plugin_status
 oncleanup(void)
 {
-    message(LDPL_INFO, "[%s] now aggregating metadata in '%s'...", tool_name, link_output_name);
+    message(L_INFO, "[%s] now aggregating metadata in '%s'...", tool_name, link_output_name);
 
     char buf[256];
 
     pid_t pid = fork();
     if (pid < 0) {
-        message(LDPL_FATAL, "[%s] fork failed: %s", tool_name, strerror_r(errno, buf, sizeof(buf)));
+        message(L_ERROR, "[%s] fork failed: %s", tool_name, strerror_r(errno, buf, sizeof(buf)));
         return LDPS_ERR;
     }
 
@@ -59,26 +88,27 @@ oncleanup(void)
             char option[ARG_MAX];
             snprintf(option, sizeof(option), "--%s%s", FILE_PREFIX_MAP_OPTION, shrink_rule);
             char *args[] = {"esstra", "shrink", option, filename, NULL};
-            message(LDPL_INFO, "[%s] invoking: '%s %s %s %s'...",
-                    tool_name, args[0], args[1], args[2], args[3]);
+            message(L_DEBUG, "> invoking: '%s %s %s %s'...", args[0], args[1], args[2], args[3]);
             execvp(ESSTRA_UTIL_COMMAND, args);
         } else {
             char *args[] = {"esstra", "shrink", filename, NULL};
-            message(LDPL_INFO, "[%s] invoking: '%s %s %s'...",
-                    tool_name, args[0], args[1], args[2]);
+            message(L_DEBUG, "> invoking: '%s %s %s'...", args[0], args[1], args[2]);
             execvp(ESSTRA_UTIL_COMMAND, args);
         }
         // below runs only on error
-        message(LDPL_FATAL, "[%s] execvp failed: %s", tool_name, strerror_r(errno, buf, sizeof(buf)));
+        message(L_ERROR, "[%s] execvp failed: %s", tool_name, strerror_r(errno, buf, sizeof(buf)));
         exit(1);
     } else {
         int status;
         waitpid(pid, &status, 0);
         if (WIFEXITED(status)) {
             int exitcode = WEXITSTATUS(status);
+            message(L_DEBUG, "> 'esstra shrink' exited with code %d", exitcode);
             if (exitcode == 0) {
-                message(LDPL_INFO, "[%s] metadata in '%s' successfully updated", tool_name, link_output_name);
+                message(L_INFO, "[%s] metadata in '%s' successfully updated", tool_name, link_output_name);
             } else {
+                message(L_ERROR, "[%s] ESSTRA Utility failed with code %d",
+                        tool_name, link_output_name, exitcode);
                 retcode = LDPS_ERR;
             }
         }
@@ -96,14 +126,12 @@ onload(struct ld_plugin_tv *tv)
     enum ld_plugin_status status;
     ld_plugin_register_cleanup register_cleanup = NULL;
 
-    fprintf(stderr, "[%s] loaded: v%s\n", tool_name, tool_version);
-
     p = tv;
 
     while (p->tv_tag) {
         switch (p->tv_tag) {
         case LDPT_MESSAGE:
-            message = p->tv_u.tv_message;
+            plugin_message = p->tv_u.tv_message;
             status = LDPS_OK;
             break;
         case LDPT_REGISTER_CLEANUP_HOOK:
@@ -115,18 +143,34 @@ onload(struct ld_plugin_tv *tv)
             status = LDPS_OK;
             break;
         case LDPT_OPTION:
+        {
             const char *option = p->tv_u.tv_string;
-            // message(LDPL_INFO, "option [%s]", option);
-            if (strncmp(option, FILE_PREFIX_MAP_OPTION, strlen(FILE_PREFIX_MAP_OPTION)) != 0) {
-                message(LDPL_FATAL, "[%s] invalid option", option);
+            message(L_DEBUG, "> option '%s'", option);
+            if (strncmp(option, FILE_PREFIX_MAP_OPTION, strlen(FILE_PREFIX_MAP_OPTION)) == 0) {
+                exists_shrink_option = true;
+                size_t remaining = sizeof(shrink_rule) - strlen(shrink_rule) - 1;
+                strncat(shrink_rule, option + strlen(FILE_PREFIX_MAP_OPTION), remaining);
+                message(L_DEBUG, "> shrink_rule: '%s'", shrink_rule);
+                status = LDPS_OK;
+            } else if (strcmp(option, "debug") == 0) {
+                messages_to_show |= L_DEBUG;
+                message(L_DEBUG, "> debug mode enabled");
+            } else if (strcmp(option, "verbose") == 0) {
+                messages_to_show |= L_ERROR | L_NOTICE | L_INFO;
+                message(L_DEBUG, "> verbose mode enabled");
+            } else if (strcmp(option, "silent") == 0) {
+                messages_to_show &= ~(L_ERROR | L_INFO | L_DEBUG);
+                message(L_DEBUG, "> silent mode enabled");
+            } else if (strcmp(option, "show-error") == 0) {
+                messages_to_show |= L_ERROR;
+                message(L_DEBUG, "> show errors");
+            } else {
+                message(L_ERROR, "[%s] invalid option", option);
                 return LDPS_ERR;
             }
-            exists_shrink_option = true;
-            size_t remaining = sizeof(shrink_rule) - strlen(shrink_rule) - 1;
-            strncat(shrink_rule, option + strlen(FILE_PREFIX_MAP_OPTION), remaining);
-            // message(LDPL_INFO, "shrink_rule: '%s'", shrink_rule);
-            status = LDPS_OK;
             break;
+        }
+
         default:
             break;
         }
@@ -136,10 +180,12 @@ onload(struct ld_plugin_tv *tv)
     if (register_cleanup) {
         status = register_cleanup(oncleanup);
         if (status != LDPS_OK) {
-            message(LDPL_FATAL, "[%s] register cleanup hook failed", tool_name);
+            message(L_ERROR, "[%s] register cleanup hook failed", tool_name);
             return status;
         }
     }
+
+    message(L_INFO, "[%s] loaded: %s", tool_name, tool_version);
 
     return status;
 }
